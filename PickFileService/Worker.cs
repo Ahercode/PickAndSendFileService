a04df32e390e8b352d94692246cf3cce66b7ca64
Ahercode <philipkelly407@gmail.com>
2024-10-04 16:02:40 +0000

    did some clean ups

--- PickFileService/Worker.cs
+++ PickFileService/Worker.cs
@@ -16,7 +16,9 @@ public class Worker : BackgroundService
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         
-        // File paths for each time slot
+        // File paths for each time slot to send the file
+        // The file paths are hard coded for demonstration purposes
+        // In a real-world scenario, I will read these from a configuration file or database
         var fileToSendAt10Am = "/Users/philip/Desktop/TestFileUpload1/";
         var fileToSendAt1220Pm = "/Users/philip/Desktop/TestFileUpload2/";
         var fileToSendAt530Pm = "/Users/philip/Desktop/TestFileUpload3/";
@@ -26,37 +28,33 @@ public class Worker : BackgroundService
         {
             var currentTime = DateTime.Now.TimeOfDay;
             
-            //
-            //
-            // // Check if it's time to send the file at 10:00 AM
-            // if (currentTime.Hours == 18 && currentTime.Minutes == 32)
-            // {
-            //     _sendFileService.CheckFolderAndSendFile(fileToSendAt10Am, "VAG Data");
-            // }
-            // // Check if it's time to send the file at 12:20 PM
-            // else if (currentTime.Hours == 18 && currentTime.Minutes == 34)
-            // {
-            //     _sendFileService.CheckFolderAndSendFile(fileToSendAt1220Pm, "Noon Data");
-            // }
-            // // Check if it's time to send the file at 5:30 PM
-            // else if (currentTime.Hours == 18 && currentTime.Minutes == 36)
-            // {
-            //    
-            //     _sendFileService.CheckFolderAndSendFile(fileToSendAt530Pm, "Daywa Data");
-            // }
-            // // Check if it's time to send the file at 7:00 PM
-            // else if (currentTime.Hours == 18 && currentTime.Minutes == 38)
-            // {
-            //  
-            //     _sendFileService.CheckFolderAndSendFile(fileToSendAt7Pm, "Original Data");
-            // }
-            
-            _sendFileService.CheckFolderAndSendFile(fileToSendAt10Am, "VAG Data");
+            // Check if it's time to send the file at 10:00 AM
+            if (currentTime.Hours == 18 && currentTime.Minutes == 32)
+            {
+                _sendFileService.CheckFolderAndSendFile(fileToSendAt10Am, "VAG Data");
+            }
+            // Check if it's time to send the file at 12:20 PM
+            else if (currentTime.Hours == 18 && currentTime.Minutes == 34)
+            {
+                _sendFileService.CheckFolderAndSendFile(fileToSendAt1220Pm, "Noon Data");
+            }
+            // Check if it's time to send the file at 5:30 PM
+            else if (currentTime.Hours == 18 && currentTime.Minutes == 36)
+            {
+                _sendFileService.CheckFolderAndSendFile(fileToSendAt530Pm, "Daywa Data");
+            }
+            // Check if it's time to send the file at 7:00 PM
+            else if (currentTime.Hours == 18 && currentTime.Minutes == 38)
+            {
+                _sendFileService.CheckFolderAndSendFile(fileToSendAt7Pm, "Original Data");
+            }
+            else
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            }
 
-            _logger.LogInformation("Current Hour: {time}", currentTime.Hours);
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            // Delay for 1 minute to avoid multiple checks within the same minute
-            await Task.Delay(1000, stoppingToken);
+            // Wait for 1 minute before checking again
+            await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
         }
     }
 }
